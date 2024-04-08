56cabea7e1c4470e35bcda9a1d13f3c74548bca4
Nasim Sahebi <sogol.sahebi@mail.utoronto.ca>
2024-04-08 18:29:41 -0400

    all scripts and files updated

--- scripts/data_processing.py
+++ scripts/data_processing.py
@@ -1,8 +1,6 @@
+import math
+
 import pandas as pd
-from sklearn.feature_extraction.text import \
-    TfidfVectorizer  # For converting text to numerical data
-from sklearn.preprocessing import OneHotEncoder, \
-    MinMaxScaler  # For encoding categorical variables and normalizing numerical data
 
 
 def to_numeric(s):
@@ -23,14 +21,14 @@ def parse_and_update_q6(row):
 
     if pd.isnull(entry) or entry == "":
         for category in categories:
-            row[f'Q6_{category}'] = default_value
+            row[f'{category}'] = default_value
     else:
         for pair in entry.split(','):
             parts = pair.split('=>')
             if len(parts) == 2 and parts[1].isdigit():
-                row[f'Q6_{parts[0]}'] = int(parts[1])
+                row[f'{parts[0]}'] = int(parts[1])
             else:
-                row[f'Q6_{parts[0]}'] = default_value
+                row[f'{parts[0]}'] = default_value
 
     return row
 
@@ -46,11 +44,11 @@ def clean_data(data):
 
     numerical_cols = ['Q1', 'Q2', 'Q3', 'Q4', 'Q7', 'Q8', 'Q9']
     for col in numerical_cols:
-        data[col].fillna(data[col].median(), inplace=True)
+        data[col] = data[col].fillna(data[col].median())
 
     categorical_cols = ['Q5', 'Q10']
     for col in categorical_cols:
-        data[col].fillna('Unknown', inplace=True)
+        data[col] = data[col].fillna('Unknown')
 
     data = data.apply(parse_and_update_q6, axis=1)
     for col in numerical_cols:
@@ -71,11 +69,106 @@ def clean_data(data):
     data_clean['Q7'] = data_clean['Q7'].clip(lower=0, upper=55)
     data_clean['Q8'] = data_clean['Q8'].clip(lower=1, upper=9.5)
     data_clean['Q9'] = data_clean['Q9'].clip(lower=0, upper=20.5)
-    for column in ['Q6_Skyscrapers', 'Q6_Sport', 'Q6_Art and Music',
-                   'Q6_Carnival', 'Q6_Cuisine', 'Q6_Economic']:
+    for column in ['Skyscrapers', 'Sport', 'Art and Music',
+                   'Carnival', 'Cuisine', 'Economic']:
         data_clean[column] = data_clean[column].clip(
             lower=0)
     return data_clean
+
+
+def one_hot_transform(cat, cols, prefix):
+    """
+    Transform data into a one-hot vector, returns it
+    :return:
+    """
+    one_hot_enc = {f"{prefix}_{category}": [] for category in cat}
+    for entry in cols:
+        for category in cat:
+            one_hot_enc[f"{prefix}_{category}"].append(int(entry == category))
+    return pd.DataFrame(one_hot_enc)
+
+
+def minmax_scaler_transform(data):
+    """
+    Transform data matrix by min max scaling
+    :return:
+    """
+    scale_data = data.copy()
+    for col in scale_data.columns:
+        p = scale_data[col]
+        max_val = p.max()
+        min_val = p.min()
+        scale_data[col] = (p - min_val) / (max_val - min_val)
+    return scale_data
+
+
+def compute_freq(string: str):
+    """
+    Compute term frequency
+    :return:
+    """
+    tf_dict = {}
+    words = string.split()
+    for word in words:
+        if word in tf_dict:
+            tf_dict[word] += 1
+        else:
+            tf_dict[word] = 1
+
+    all_terms = len(words)
+    for word in tf_dict:
+        tf_dict[word] /= all_terms
+    return tf_dict
+
+
+def compute_inverse_text_freq(text_list):
+    idf_dict = {}
+    n = len(text_list)
+
+    for text in text_list:
+        for word in set(text.split()):
+            if word in idf_dict:
+                idf_dict[word] += 1
+            else:
+                idf_dict[word] = 1
+
+    for word in idf_dict:
+        idf_dict[word] = math.log(n + 1 / idf_dict[word] + 1) + 1
+
+    return idf_dict
+
+
+def tfidf_vectorizer(all_texts, max_features=150):
+    """
+    Uses TFID to vectorize words
+    :return:
+    """
+    tfidf_list = []
+    idfs = compute_inverse_text_freq(all_texts)
+
+    for text in all_texts:
+        tf = compute_freq(text)
+        tfidf_dict = {}
+        for word, t_value in tf.items():
+            tfidf_dict[word] = t_value * idfs.get(word, 0)
+        tfidf_list.append(tfidf_dict)
+
+    all_tfidf = {}
+    for txt_tfidf in tfidf_list:
+        for word, score in txt_tfidf.items():
+            if word in all_tfidf:
+                all_tfidf[word] += score
+            else:
+                all_tfidf[word] = score
+
+    top_features = sorted(all_tfidf, key=all_tfidf.get, reverse=True)[
+                   :max_features]
+    filtered_tfidfs = []
+    for tfidfs in tfidf_list:
+        filtered_tfidf = {word: score for word, score in tfidfs.items() if
+                          word in top_features}
+        filtered_tfidfs.append(filtered_tfidf)
+    return filtered_tfidfs
 
 
 def normalize_data(data):
@@ -85,40 +178,36 @@ def normalize_data(data):
     :return:
     """
     processed_data = data.copy()
-    scaler = MinMaxScaler()
     numerical_cols = ['Q1', 'Q2', 'Q3', 'Q4', 'Q7', 'Q8', 'Q9',
-                      'Q6_Skyscrapers', 'Q6_Sport', 'Q6_Art and Music',
-                      'Q6_Carnival',
-                      'Q6_Cuisine', 'Q6_Economic']
+                      'Skyscrapers', 'Sport', 'Art and Music',
+                      'Carnival',
+                      'Cuisine', 'Economic']
     processed_data[numerical_cols] = processed_data[numerical_cols].fillna(0)
 
-    processed_data[numerical_cols] = scaler.fit_transform(
+    processed_data[numerical_cols] = minmax_scaler_transform(
         processed_data[numerical_cols])
 
-    one_hot_encoder = OneHotEncoder()
-    q5_encoded = one_hot_encoder.fit_transform(
-        processed_data[['Q5']]).toarray()  # Directly convert to array here
-
-    feature_names = one_hot_encoder.get_feature_names_out(
-        input_features=['Q5'])
-    for i, column_name in enumerate(feature_names):
-        processed_data[column_name] = q5_encoded[:, i]
+    categories = processed_data['Q5'].unique()
+    q5_encoded_df = one_hot_transform(categories, processed_data['Q5'],
+                                      prefix="Q5")
+    processed_data = pd.concat([processed_data, q5_encoded_df], axis=1)
 
     # Q10: Convert textual data in Q10 into numerical format using TF-IDF
-    tfidf_vectorizer = TfidfVectorizer(max_features=500)
-    q10_tfidf = tfidf_vectorizer.fit_transform(processed_data['Q10']).toarray()
+    q10_words = processed_data['Q10'].fillna("").tolist()
+    q10_tfidf = tfidf_vectorizer(q10_words[:150])
 
     # Get feature names from TF-IDF vectorizer
-    feature_names = tfidf_vectorizer.get_feature_names_out()
+    q10_tfidf_df = pd.DataFrame.from_records(q10_tfidf).fillna(0)
+    q10_tfidf_df.columns = ['Q10_tfidf_' + str(col) for col in
+                            q10_tfidf_df.columns]
 
     # Create DataFrame from TF-IDF vectors and concatenate with processed_data
-    q10_tfidf_df = pd.DataFrame(q10_tfidf,
-                                columns=['Q10_tfidf_' + name for name in
-                                         feature_names])
     processed_data = pd.concat([processed_data.reset_index(drop=True),
-                                q10_tfidf_df.reset_index(drop=True)], axis=1)
+                                q10_tfidf_df.reset_index(drop=True)],
+                               axis=1)
 
     processed_data.drop(['id', 'Q5', 'Q6', 'Q10'], axis=1, inplace=True)
+    processed_data.fillna(0, inplace=True)
 
     # Renaming columns for clarity
     processed_data.rename(columns={
@@ -144,16 +233,19 @@ def feature_choice(data):
         column for column in data.columns if "Q10" in column
     ]
     additional_columns_keep = [
-        "Viral Potential",  # Q2
-        "Avg Temp Jan",  # Q7
-        "Q6_Economic",
-        'Q6_Skyscrapers',
-        "Label",
-        "Fashion Styles"
-        # "Sport",
-        # 'Art and Music',
-        # 'Carnival',
-        # 'Economic'
+        "Viral Potential",
+        "Avg Temp Jan",
+        "Economic",
+        'Skyscrapers',
+        # "Label",
+        "Q5_Partner",
+        "Q5_Partner,Friends",
+        "Q5_Friends",
+        "Sport",
+        'Art and Music',
+        'Carnival',
+        "Architectural Uniqueness",
+        "Languages Overheard"
     ]
 
     columns_all_keep = list(
--- scripts/pred.py
+++ scripts/pred.py
@@ -1,27 +1,37 @@
 import numpy as np
-from model import softmax
-from data_processing import clean_data, normalize_data, feature_choice
 import pandas as pd
-from model import grad_descent
+from data_processing import clean_data, feature_choice, normalize_data
+from model import softmax
+
+
+def read_weights_from_file(filename):
+    weights = []
+    with open(filename, 'r') as file:
+        for line in file:
+            new_line = line.strip().replace('[', '').replace(']', '').replace(
+                ',', ' ')
+            row = [float(num) for num in new_line.split()]
+            weights.append(row)
+    return np.array(weights)
+
+
+WEIGHTS = read_weights_from_file('weights.txt')
 
 
 def process_data(data):
     data = clean_data(data)
-    data = normalize_data(data)
-    X = feature_choice(data)
-    x_train = X.drop(["Label"], axis=1).values
-    labels = pd.get_dummies(X["Label"])
-    t_train = labels.to_numpy()
-    weights = grad_descent(X_train=x_train, t_train=t_train, alpha=0.1, n_iter=30)
-    label_mapping = {i: label for i, label in enumerate(labels.columns)}
-    return x_train, weights, label_mapping
+    x = normalize_data(data)
+    x_features = feature_choice(x)
+    return x_features
 
 
 def predict_all(filename: str):
     data = pd.read_csv(filename)
-    X, weights, label_mapping = process_data(data)
-    z = np.dot(X, weights)
+    x = process_data(data)
+    z = np.dot(x, WEIGHTS)
     prob = softmax(z)
     predictions = np.argmax(prob, axis=1)
-    prediction_labels = [label_mapping[pred] for pred in predictions]
-    return prediction_labels
+
+    # Map indices to city names
+    cities = ['Dubai', 'Rio de Janeiro', 'New York City', 'Paris']
+    return [cities[idx] for idx in predictions]
